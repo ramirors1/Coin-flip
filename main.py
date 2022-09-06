7dde612196394a064de8fbd88a83055458af8a90
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-09-06 14:49:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+number = random.randint(1, 2)
+
+if number == 1:
+  print("Heads")
+else:
+  print("Tails")
+  
+
+
+
+
+
+
+
+
+
